ab8b17fa279335347f51943a5231977d100b88cf
Romain <romain.pillot@epitech.eu>
2017-05-28 21:43:35 +0000

    Finishing config loader

--- sources/my_create_objects.c
+++ sources/my_create_objects.c
@@ -5,10 +5,13 @@
 ** Login   <antonin.rapini@epitech.net>
 ** 
 ** Started on  Wed Mar  8 17:28:16 2017 Antonin Rapini
-** Last update Sun May 28 20:42:11 2017 romain pillot
+** Last update Sun May 28 21:34:28 2017 romain pillot
 */
 
+#include <stdlib.h>
+#include <unistd.h>
 #include "sources.h"
+#include "utils.h"
 
 static t_object	*create_object(t_key *key)
 {
@@ -16,51 +19,49 @@ static t_object	*create_object(t_key *key)
 
   if (!(object = malloc(sizeof(t_object))))
     return (NULL);
-  object->type = keyget_integer("type");
-  object->position = my_create_sfvector3f(keyget_integer("position.x"),
-					  keyget_integer("position.y"),
-					  keyget_integer("position.z"));
-  object->rotation = my_create_sfvector3f(keyget_integer("rotation.x"),
-					  keyget_integer("rotation.y"),
-					  keyget_integer("rotation.z"));
-  object->info = keyget_integer("info");
-  object->color.r = keyget_integer("color.r");
-  object->color.g = keyget_integer("color.g");
-  object->color.b = keyget_integer("color.b");
-  object->color.a = keyget_integer("color.a");
-  object->brillance = keyget_integer("brillance");
-  object->reflection = keyget_integer("reflection");
-  object->transparency = keyget_integer("transparency");
-  object->refraction_i = keyget_integer("refraction_i");
+  object->type = keyget_integer(key, "type");
+  object->position = my_create_sfvector3f(keyget_integer(key, "position.x"),
+					  keyget_integer(key, "position.y"),
+					  keyget_integer(key, "position.z"));
+  object->rotation = my_create_sfvector3f(keyget_integer(key, "rotation.x"),
+					  keyget_integer(key, "rotation.y"),
+					  keyget_integer(key, "rotation.z"));
+  object->info = keyget_integer(key, "info");
+  object->color.r = keyget_integer(key, "color.r");
+  object->color.g = keyget_integer(key, "color.g");
+  object->color.b = keyget_integer(key, "color.b");
+  object->color.a = keyget_integer(key, "color.a");
+  object->brillance = keyget_integer(key, "brillance");
+  object->reflection = keyget_integer(key, "reflection");
+  object->transparency = keyget_integer(key, "transparency");
+  object->refraction_i = keyget_integer(key, "refraction_i");
   return (object);
 }
 
-/*
-** yolo, dats temporary
-*/
-static t_object	*convert(t_array *array, t_key **keys, int length)
+/* temporary */
+static t_object	*convert(t_array *array, t_object **objs, int length)
 {
   int		i;
-  t_array	*new;
+  t_object	*new;
 
-  len = array->length;
-  new = malloc(sizeof(t_object * (length + 1)));
-  new[len].type = 0;
+  if (!(new = malloc(sizeof(t_object) * (length + 1))))
+    return (NULL);
+  new[length].type = 0;
   i = -1;
-  while (++i < len)
+  while (++i < length)
     {
-      new[i].type = keys[i]->type;
-      new[i].position = keys[i]->position;
-      new[i].rotation = keys[i]->rotation;
-      new[i].info = keys[i]->info;
-      new[i].color.r = keys[i]->color.r;
-      new[i].color.g = keys[i]->color.g;
-      new[i].color.b = keys[i]->color.b;
-      new[i].color.a = keys[i]->color.a;
-      new[i].brillance = keys[i]->brillance;
-      new[i].reflection = keys[i]->reflection;
-      new[i].transparency = keys[i]->transparency;
-      new[i].refraction_i = keys[i]->refraction_i;
+      new[i].type = objs[i]->type;
+      new[i].position = objs[i]->position;
+      new[i].rotation = objs[i]->rotation;
+      new[i].info = objs[i]->info;
+      new[i].color.r = objs[i]->color.r;
+      new[i].color.g = objs[i]->color.g;
+      new[i].color.b = objs[i]->color.b;
+      new[i].color.a = objs[i]->color.a;
+      new[i].brillance = objs[i]->brillance;
+      new[i].reflection = objs[i]->reflection;
+      new[i].transparency = objs[i]->transparency;
+      new[i].refraction_i = objs[i]->refraction_i;
     }
   array_destroy(&array, true);
   return (new);
@@ -69,16 +70,16 @@ static t_object	*convert(t_array *array, t_key **keys, int length)
 t_object	*my_create_objects(t_config *config)
 {
   t_key		*objects;
-  t_key		**keys;
+  t_array	*keys;
   t_array	*array;
   int		i;
 
   if (!(array = array_create()))
     return (NULL);
   objects = get_key(config, "objects");
-  keys = (t_key **) objects->value;
+  keys = (t_array *) objects->value;
   i = -1;
-  while (keys[++i])
-    array_add(array, create_object(keys[i]));
-  return (convert(array, (t_key **)array->values, array->length));
+  while (((t_key **) keys->values)[++i])
+    array_add(array, create_object((t_key *) keys->values[i]));
+  return (convert(array, (t_object **)array->values, array->length));
 }
