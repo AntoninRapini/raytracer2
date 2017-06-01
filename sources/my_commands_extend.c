e5b19cf3b17e68598282974f5689903099e2f763
Raphaël Goulmot <raphael.goulmot@epitech.eu>
2017-06-01 10:29:26 +0200

    debug

--- sources/my_commands_extend.c
+++ sources/my_commands_extend.c
@@ -5,7 +5,7 @@
 ** Login   <raphael.goulmot@epitech.net>
 ** 
 ** Started on  Wed May 31 11:11:51 2017 Raphaël Goulmot
-** Last update Thu Jun  1 10:23:37 2017 Raphaël Goulmot
+** Last update Thu Jun  1 10:29:19 2017 Raphaël Goulmot
 */
 
 #include "sources.h"
@@ -13,14 +13,7 @@
 
 void	rotate_object(t_scene *scene, t_object *obj, int key)
 {
-  sfVector3f	new;
-  sfVector3f	rot;
-
-  key = key;
-  new = my_create_sfvector3f(obj->rotation.x
-			     , obj->rotation.y, obj->rotation.z);
-  rot = my_create_sfvector3f(0, new.y + (sfKeyI == key
-				? 5 : sfKeyK == key ? -5 : 0), 0);
-  obj->rotation = rotate_zyx(new, rot);
+  obj->rotation.y += sfKeyL == key ? -5 : sfKeyJ == key ? 5 : 0;
+  obj->rotation.x += sfKeyI == key ? 5 : sfKeyK == key ? -5 : 0;
   scene = scene;
 }
